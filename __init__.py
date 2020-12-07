2e6d445b614ac0cf22ebf7630dab8488b3fe64e7
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-07 12:07:26 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,20 +1,19 @@
 # This file is part product_variant module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
 from trytond.pool import Pool
-from .configuration import *
-from .product import *
+from . import configuration
+from . import product
 
 
 def register():
     Pool.register(
-        Configuration,
-        ConfigurationCodeSeparator,
-        Product,
-        Template,
-        ProductAttribute,
-        AttributeValue,
-        ProductTemplateAttribute,
-        ProductAttributeValue,
+        configuration.Configuration,
+        configuration.ConfigurationCodeSeparator,
+        product.Product,
+        product.Template,
+        product.ProductAttribute,
+        product.AttributeValue,
+        product.ProductTemplateAttribute,
+        product.ProductAttributeValue,
         module='product_variant', type_='model')
